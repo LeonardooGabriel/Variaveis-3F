9da5cce94aca24d323c49b37cc5ac79c1061a8cf
Leonardo Gabriel <126363100+LeonardooGabriel@users.noreply.github.com>
2024-08-12 23:09:27 +0000

    12/08/2024-2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const idade = 18;
+let idade = 18;
 const anoNascimento = 2006;
 let anoAtual = 2024;
+const nomeCompleto = "Leonardo Gabriel Matias De OLiveira";
+
+console.log("Meu nome é " + nomeCompleto + " nascido no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos");
 
 anoAtual = anoAtual + 1;
+idade = idade +1;
 
 console.log(anoAtual);
+console.log(idade);
